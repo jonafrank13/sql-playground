703b4046f36ba2c28484098f7b2817179d18e57f
Jona Frank <jona.mailbox@gmail.com>
2024-06-03 02:50:50 +0530

    feat: scaffold project

---
+++ src/components/SQLEditor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <CodePrism :lang="props.lang" :code="props.code" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CodePrism from './CodePrism';
+
+const props = defineProps({
+  code: String,
+
+  lang: {
+    type: String,
+    default: 'sql',
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-input v-model="textareaModel" filled type="textarea" />
+    <SQLEditor :code="textareaModel" />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import SQLEditor from 'components/SQLEditor.vue';
+
+defineOptions({
+  name: 'IndexPage',
+});
+
+const textareaModel = ref('');
+</script>
